645f653b50a2686342613cee904daaacfe804700
Anda Cabrera <andarcabrera@gmail.com>
2016-01-25 10:54:42 -0600

    Add human_player spot available validation

--- lib/ttt/players/human_player.rb
+++ lib/ttt/players/human_player.rb
@@ -2,7 +2,9 @@ require_relative 'player'
 
 class HumanPlayer < Player
 
-  def pick_spot(user_input)
-    1
+  def pick_spot(board, user_input)
+    if board.available_spot(user_input)
+      user_input
+    end
   end
 end
--- spec/human_player_spec.rb
+++ spec/human_player_spec.rb
@@ -5,9 +5,20 @@ require_relative '../lib/ttt/players/human_player'
 
   describe "pick_spot" do
 
-  let (:human_player) { HumanPlayer.new("Anda", "X") }
-    it "picks a spot to place its marker" do
+    let (:human_player) { HumanPlayer.new("Anda", "X") }
+    let (:board) { double 'board' }
 
-      expect(human_player.pick_spot(1)).to eq(1)
+    it "picks a spot to place its marker" do
+      allow(board).to receive(:available_spot).and_return(true)
+      user_input = 5
+
+      expect(human_player.pick_spot(board, user_input)).to eq(5)
+    end
+
+    it "returns nil if spot is not available on the board" do
+      allow(board).to receive(:available_spot).and_return(false)
+      user_input = 5
+
+      expect(human_player.pick_spot(board, user_input)).to eq(nil)
     end
   end
