edb55e78ab0a7b7d252a12fb5166d5c0895f72ae
Anda <andarcabrera@gmail.com>
2016-01-29 13:32:11 -0600

    Require new files

--- lib/ttt.rb
+++ lib/ttt.rb
@@ -1,3 +1,6 @@
+require_relative 'ttt/players/player'
+require_relative 'ttt/players/human_player'
+require_relative 'ttt/players/computer_player'
 require_relative 'ttt/board'
 require_relative 'ttt/game'
 require_relative 'ttt/computer_move'
