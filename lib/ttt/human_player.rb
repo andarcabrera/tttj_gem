a8c5b570ae49c5aeb8dec4a5b12e2da73c73fdfd
Anda <andarcabrera@gmail.com>
2016-02-01 22:46:18 -0600

    reset spot in human move

--- lib/ttt/human_player.rb
+++ lib/ttt/human_player.rb
@@ -5,6 +5,7 @@ module TTT
     def pick_spot(board)
       spot = @ui.get_spot
       until board.available_spot(spot)
+        spot = nil
         break if @ui.no_more_moves_allowed?
         @ui.error
         spot = @ui.get_spot
